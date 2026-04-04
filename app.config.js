0799942031f55663fdf81dfae81c324b852001ca
Samba Ba <basamba1990@yahoo.fr>
2026-04-04 22:01:06 +0000

    update

--- app.config.js
+++ app.config.js
@@ -2,9 +2,18 @@ export default {
   expo: {
     name: "TontineG-One",
     slug: "tontineone",
+
+    android: {
+      package: "com.basamba1990.tontineone"
+    },
+
     plugins: ["expo-router"],
+
     extra: {
-      EXPO_ROUTER_APP_ROOT: "app"
+      EXPO_ROUTER_APP_ROOT: "app",
+      eas: {
+        projectId: "e6c0dbc2-ddc3-47c1-a01f-7ccbfc64c5b0"
+      }
     }
   }
 };
